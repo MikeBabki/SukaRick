bbfb0906ba5294278bcf79e0d2ed4748c09733d8
Makar <113338407+MikeBabki@users.noreply.github.com>
2023-01-18 14:39:48 +0500

    Initial Commit

---
+++ SukaRick/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SukaRick
+//
+//  Created by Макар Тюрморезов on 18.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
